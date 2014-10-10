95f2c5246c7ba87d7d0b3fac94e25ef0c49855ee
yechunan <644458@qq.com>
2014-10-10 09:54:31 +0800

    Framework init

---
+++ app/config/packages/chumper/datatable/config.php
@@ -0,0 +1,156 @@
+<?php
+
+return array(
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table specific configuration options.
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'table' => array(
+
+        /*
+        |--------------------------------------------------------------------------
+        | Table class
+        |--------------------------------------------------------------------------
+        |
+        | Class(es) added to the table
+        | Supported: string
+        |
+        */
+
+        'class' => 'table table-bordered table-hover',
+
+        /*
+        |--------------------------------------------------------------------------
+        | Table ID
+        |--------------------------------------------------------------------------
+        |
+        | ID given to the table. Used for connecting the table and the Datatables
+        | jQuery plugin. If left empty a random ID will be generated.
+        | Supported: string
+        |
+        */
+
+        'id' => '',
+
+        /*
+        |--------------------------------------------------------------------------
+        | DataTable options
+        |--------------------------------------------------------------------------
+        |
+        | jQuery dataTable plugin options. The array will be json_encoded and
+        | passed through to the plugin. See https://datatables.net/usage/options
+        | for more information.
+        | Supported: array
+        |
+        */
+
+        'options' => array(
+            "oLanguage" => array(
+                "sLengthMenu" => "每页显示 _MENU_ 条记录",
+                "sInfo" => "从 _START_ 到 _END_ /共 _TOTAL_ 条数据",
+                "sInfoEmpty" => "没有数据",
+                "sInfoFiltered" => "(从 _MAX_ 条数据中检索)",
+                "oPaginate" => array(
+                    "sFirst" => "首页",
+                    "sPrevious" => "前一页",
+                    "sNext" => "后一页",
+                    "sLast" => "尾页"
+                ),
+                "sSearch" => "搜索：",
+                "sZeroRecords" => "没有检索到数据"
+            ),
+//            "sPaginationType" => "full_numbers",
+            "bProcessing" => false,
+            "bPaginate" => true,
+            "bLengthChange" => true,
+            "bFilter" => true,
+            "bSort" => false,
+            "bInfo" => true,
+            "bAutoWidth" => false
+        ),
+
+        /*
+        |--------------------------------------------------------------------------
+        | DataTable callbacks
+        |--------------------------------------------------------------------------
+        |
+        | jQuery dataTable plugin callbacks. The array will be json_encoded and
+        | passed through to the plugin. See https://datatables.net/usage/callbacks
+        | for more information.
+        | Supported: array
+        |
+        */
+
+        'callbacks' => array(),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Skip javascript in table template
+        |--------------------------------------------------------------------------
+        |
+        | Determines if the template should echo the javascript
+        | Supported: boolean
+        |
+        */
+
+        'noScript' => false,
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Table view
+        |--------------------------------------------------------------------------
+        |
+        | Template used to render the table
+        | Supported: string
+        |
+        */
+
+        'table_view' => 'datatable::template',
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | Script view
+        |--------------------------------------------------------------------------
+        |
+        | Template used to render the javascript
+        | Supported: string
+        |
+        */
+
+        'script_view' => 'datatable::javascript',
+
+
+    ),
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Engine specific configuration options.
+    |--------------------------------------------------------------------------
+    |
+    */
+
+    'engine' => array(
+
+        /*
+        |--------------------------------------------------------------------------
+        | Search for exact words
+        |--------------------------------------------------------------------------
+        |
+        | If the search should be done with exact matching
+        | Supported: boolean
+        |
+        */
+
+        'exactWordSearch' => false,
+
+    )
+
+
+);
