65d749f0d00d3aafd8d44c99a6cd9f00752f5b0f
yechunan <644458@qq.com>
2014-12-16 13:32:25 +0800

    新增头像上传功能

--- public/static/js/views/ucenter/article/article.js
+++
@@ -1,9 +0,0 @@
-define(['jquery', 'jquery.easing', 'stickUp', 'bootstrap'], function($){
-	$('.navbar-osk').stickUp({
-		topMargin: -79
-	});
-
-	$('.edit-btn').on('click', function(e) {
-		location.href = $(this).data("url");
-	});
-});
